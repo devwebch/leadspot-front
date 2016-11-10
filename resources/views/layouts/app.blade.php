e095566040921eb29ed603cbb056bf285011d4a3
Simon Rapin <srapin@novahealthsa.com>
2016-11-10 15:24:03 +0100

    integrate GA on production only

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,6 +65,7 @@
     @yield('styles')
 </head>
 <body class="pace-complete @yield('body-class')">
+@if( App::environment('production') )
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -75,6 +76,7 @@
     ga('send', 'pageview');
 
 </script>
+@endif
 <!-- BEGIN HEADER -->
 @section('header')
     @include('layouts.partials.header-transparent')
