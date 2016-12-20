4189f89dacee2752d35105e352c85a7f605bac06
Simon Rapin <simon.rapin@gmail.com>
2016-12-20 18:20:57 +0100

    added tawk.to online chat widget.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -117,6 +117,19 @@
 <script type="text/javascript" src="{{asset('js/modernizr.custom.js')}}" async defer></script>
 <script type="text/javascript" src="{{asset('js/all.js')}}" async defer></script>
 <script type="text/javascript" src="{{asset('js/app.js')}}" async defer></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/58596267ddb8373fd2b151a0/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 @yield('scripts')
 </body>
 </html>
